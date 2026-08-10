167e80cd5788f794d92359c384487017e3683931
Abhay-Gupta-Git <guptaabhay2618@gmail.com>
2026-08-10 14:50:46 +0530

    Time: 2 ms (32.35%), Space: 42.8 MB (52.46%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -5,12 +5,12 @@ class Solution {
             Arrays.sort(stones);
             int a=stones[n-1];
             int b=stones[n-2];
-            if (a==b) {
+            stones[n-2]=0;
+            if(a==b) {
                 stones[n-1]=0;
-                stones[n-2]=0;
+               
             }else{
-                stones[n-1]=a-b;
-                stones[n-2]=0;
+                stones[n-1]=a-b;  
             }
         }
         Arrays.sort(stones);
