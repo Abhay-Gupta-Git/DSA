11d09fed58055e3fa1339fbd775d59caf4d087d8
Abhay-Gupta-Git <guptaabhay2618@gmail.com>
2026-09-15 00:21:07 +0530

    Time: 0 ms (100%), Space: 42.9 MB (17.64%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+
+        // Rectangle 1 is completely left of Rectangle 2
+        if (rec1[2] <= rec2[0]) {
+            return false;
+        }
+
+        // Rectangle 2 is completely left of Rectangle 1
+        if (rec2[2] <= rec1[0]) {
+            return false;
+        }
+
+        // Rectangle 1 is completely below Rectangle 2
+        if (rec1[3] <= rec2[1]) {
+            return false;
+        }
+
+        // Rectangle 2 is completely below Rectangle 1
+        if (rec2[3] <= rec1[1]) {
+            return false;
+        }
+
+        return true;
+    }
+}
