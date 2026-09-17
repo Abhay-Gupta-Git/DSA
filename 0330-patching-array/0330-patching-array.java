08f2e1541b27fb8070e6b0d4ac4efa41ee44bebb
Abhay-Gupta-Git <guptaabhay2618@gmail.com>
2026-09-17 23:41:55 +0530

    Time: 0 ms (100%), Space: 45.2 MB (62.09%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int i = 0;
+        int answer = 0;
+
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss = miss + nums[i];
+                i++;
+            } else {
+                miss = miss + miss;
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
