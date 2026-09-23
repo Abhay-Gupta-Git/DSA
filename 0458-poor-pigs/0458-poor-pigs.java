94a6be9a88be16957c88e5ba085494f67907552e
Abhay-Gupta-Git <guptaabhay2618@gmail.com>
2026-09-23 23:53:43 +0530

    Time: 0 ms (100%), Space: 42 MB (54.74%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.java
+++ 0458-poor-pigs/0458-poor-pigs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int round=minutesToTest / minutesToDie;
+        int round=minutesToTest/minutesToDie;
         int state=round+1;
         int pig=0;
         int com=1;
